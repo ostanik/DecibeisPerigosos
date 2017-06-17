0b658adce6b5b7b7247ca5f9aef9811f6f022b8c
Alan Ostanik <alvitorostanik@gmail.com>
2017-06-17 17:44:22 -0300

    Primeiro Commit.

---
+++ UnityProject/Assets/Scripts/RoundData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RoundData {
+
+    public string name;
+    public int correctAnswerPoints;
+    public QuestionData[] questions;
+
+}
