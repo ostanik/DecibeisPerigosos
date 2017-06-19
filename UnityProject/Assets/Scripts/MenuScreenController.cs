46439364d74a7433e168b054b12a67f6f78aa23e
Alan Ostanik <alvitorostanik@gmail.com>
2017-06-18 21:04:55 -0300

    Calling the game Scene on click

---
+++ UnityProject/Assets/Scripts/MenuScreenController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScreenController : MonoBehaviour {
+
+	public void StartGame () {
+        StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator ChangeScene() {
+        float fadeTime = GameObject.Find("Fade_Object").GetComponent<Fade>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene("Game");
+    }
+
+}
