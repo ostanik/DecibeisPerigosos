effb0cb20fae00f93ae100cbb83f73f96734e289
Alan Ostanik <alvitorostanik@gmail.com>
2017-06-25 17:08:17 -0300

    Adding typing text effect.

---
+++ UnityProject/Assets/Scripts/UI/TextTyper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTyper : MonoBehaviour {
+
+    public float letterPause = 0.2f;
+    public AudioClip typeSound1;
+    public AudioClip typeSound2;
+    public bool isFinished = false;
+
+    string message;
+    Text textComp;
+
+    // Use this for initialization
+    void Start() {
+        textComp = GetComponent<Text>();
+        message = textComp.text;
+        textComp.text = "";
+        StartCoroutine(TypeText());
+    }
+
+    public void Finish() {
+        textComp.text = message;
+        isFinished = true;
+        StopAllCoroutines();
+    }
+
+    IEnumerator TypeText() {
+        foreach (char letter in message.ToCharArray()) {
+            textComp.text += letter;
+            if (textComp.text.Length == message.Length) {
+                isFinished = true;
+            }
+            yield return 0;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+}
